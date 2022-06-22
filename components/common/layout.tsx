949d7c6d813a725724050b0bf611246aea5ac9cd
Kingsley Zhong <kingsley@kingsleyzhong.com>
2022-06-21 22:42:25 -0600

    fixed type

--- components/common/layout.tsx
+++ components/common/layout.tsx
@@ -3,11 +3,15 @@ import React from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
 
-const Layout = ({ children }) => (
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = (props: LayoutProps) => (
   <>
     <Navbar />
     <div className="cursor-igem mt-12">
-      {children}
+      {props.children}
     <Footer />
     </div>
   </>
