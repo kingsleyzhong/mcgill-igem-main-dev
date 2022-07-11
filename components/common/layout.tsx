464e8dfd7a00d85a074ad84c443aaf82a251b450
Kingsley Zhong <kingsley@kingsleyzhong.com>
2022-07-11 05:24:35 -0600

    fixed icon and title

--- components/common/layout.tsx
+++ components/common/layout.tsx
@@ -3,12 +3,18 @@ import React from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
 
+import Head from "next/head";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = (props: LayoutProps) => (
   <>
+    <Head>
+      <title>McGill IGEM</title>
+      <link rel="icon" type="image/x-icon" href="favicon.ico" />
+    </Head>
     <div className="">
     <Navbar />
       {props.children}
